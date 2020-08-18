f63ed9dd2f8d5bed35044faf8f7d816aae46800e
nicesp <69475238+nicesp@users.noreply.github.com>
2020-08-18 17:21:49 +0200

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -42,7 +42,7 @@ if version_info[0] < 3 or version_info[1] < 6:
     quit(1)
 
 # Check if the config was edited by using the already used variable
-CONFIG_CHECK = os.environ.get("", None)
+CONFIG_CHECK = os.environ.get("___________PLOX_______REMOVE_____THIS_____LINE__________", None)
 
 if CONFIG_CHECK:
     LOGS.error("Please remove the line mentioned in the first hashtag from the config.env file")
@@ -54,7 +54,7 @@ API_HASH = os.environ.get("API_HASH", None)
 
 STRING_SESSION = os.environ.get("STRING_SESSION", None)
 
-LOGGER_GROUP = int(os.environ.get("LOGGER_GROUP", None))
+LOGGER_GROUP = int(os.environ.get("LOGGER_GROUP", "0"))
 
 LOGGER = sb(os.environ.get(
     "LOGGER", "False"
